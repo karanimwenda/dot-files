c59ea4f69485f5e3a73c8bab23b2a892eaf89ebd
Timothy Karani <kartimothy@gmail.com>
2020-10-15 10:03:17 +0300

    Added autoload file

--- init.sh
+++ init.sh
@@ -15,4 +15,42 @@ initvim() {
     echo "======================================"
 }
 
-initvim
+clean-home() {
+    echo "======================================"
+    echo "Deleting"
+    echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"
+    echo " .vimrc"
+    rm -rf ~/.vimrc 
+    # echo " .vim"
+    # rm -rf ~/.vim
+    echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"
+    echo "Done."
+    echo "======================================"
+}
+
+if [ $# -eq 0 ]
+then
+    echo "ERROR: Insufficient number of arguments passed, exiting"
+    exit 1
+
+elif [ $1 = "vim" ] 
+then
+    initvim
+
+elif [ $1 = "clean" ]
+then
+    read -p "Cleans configs in your home dir. Are you sure?(y/N): " CONFIRM
+    if [ -z  $CONFIRM ]
+    then
+        echo "Not deleting."
+    elif [ $CONFIRM = "y" ] || [ $CONFIRM = "Y" ]
+    then
+        clean-home
+   else
+        echo "Not deleting."
+    fi
+
+else
+    echo "Unknown command, exiting."
+    exit 1
+fi
