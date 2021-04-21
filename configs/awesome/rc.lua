0525b239424eb1cd81a8632441349df53d36979a
Timothy Karani <kartimothy@gmail.com>
2021-04-21 11:02:33 +0300

    Floating libgdx window

--- configs/awesome/rc.lua
+++ configs/awesome/rc.lua
@@ -686,6 +686,7 @@ awful.rules.rules = {
           "Sxiv",
           "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
           "Wpa_gui",
+          "com-badlogic-gdx-setup-GdxSetup",
           "veromix",
           "Pavucontrol",
           "xtightvncviewer"},
