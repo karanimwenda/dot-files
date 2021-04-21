cdc076c22911f12fc699eacc8a8205fe1055ab89
Timothy Karani <kartimothy@gmail.com>
2021-04-21 10:53:09 +0300

    Made magnifier the next option

--- configs/awesome/rc.lua
+++ configs/awesome/rc.lua
@@ -75,7 +75,7 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.tile,
-    awful.layout.suit.floating,
+    awful.layout.suit.magnifier,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
@@ -85,8 +85,8 @@ awful.layout.layouts = {
     awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
     awful.layout.suit.corner.nw,
+    awful.layout.suit.floating,
     -- awful.layout.suit.corner.ne,
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
