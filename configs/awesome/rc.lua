059d9af4b1e2cd432ffc8349a88fd227667b2e80
Timothy Karani <kartimothy@gmail.com>
2021-06-23 09:04:18 +0300

    Realigned layout order

--- configs/awesome/rc.lua
+++ configs/awesome/rc.lua
@@ -79,8 +79,8 @@ modkey = "Mod4"
 awful.layout.layouts = {
     awful.layout.suit.tile,
     awful.layout.suit.magnifier,
-    awful.layout.suit.tile.left,
     awful.layout.suit.tile.top,
+    awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
