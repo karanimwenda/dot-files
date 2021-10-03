e5aef6762e464e4536c48ec90d6325785ec60be1
Timothy Karani <kartimothy@gmail.com>
2021-10-03 14:54:14 +0300

    Can get today's date

---
+++ utils/eww-gcalcli/get-schedule.py
@@ -0,0 +1,14 @@
+import os
+import datetime
+
+
+today_date = datetime.datetime.now()
+tomorrow_date = today_date + datetime.timedelta(days=1)
+today = today_date.strftime("%m/%d/%Y")
+tomorrow = tomorrow_date.strftime("%m/%d/%Y")
+print(today, tomorrow)
+
+stream = os.popen(
+    'gcalcli agenda "{}" "{}" --details end'.format(today, tomorrow))
+output = stream.read()
+print(output)
