d3f1b6ac4da3a0df00f980f3bc72ede9e95da43d
Timothy Karani <kartimothy@gmail.com>
2021-10-03 17:45:51 +0300

    Can generate cal event

--- utils/eww-gcalcli/get-schedule.py
+++ utils/eww-gcalcli/get-schedule.py
@@ -1,13 +1,7 @@
 import os
 import datetime
 
-yucky_event_template = """
-(cal-event 
-    :details "{{ details }}"
-    :time "{{ time }}"
-)
-"""
-
+# Get day tasks
 today_date = datetime.datetime.now()
 tomorrow_date = today_date + datetime.timedelta(days=1)
 today = today_date.strftime("%m/%d/%Y")
@@ -39,6 +33,10 @@ for task in unprocessed_tasks:
 
 task_code = []
 
+with open("../../configs/eww/widgets/cal-event-template.yuck") as file:
+    yucky_event_template = file.read()
+
+# Create yucky code
 for task in processed_tasks:
     eww_task = yucky_event_template
     eww_task = eww_task.replace("{{ details }}", task["details"])
