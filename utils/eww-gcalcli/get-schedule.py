477c213d1b36870ded5b9dcdb03b6d8917d062d0
Timothy Karani <kartimothy@gmail.com>
2021-10-03 17:36:28 +0300

    Added cal-event widget

--- utils/eww-gcalcli/get-schedule.py
+++ utils/eww-gcalcli/get-schedule.py
@@ -1,6 +1,12 @@
 import os
 import datetime
 
+yucky_event_template = """
+(cal-event 
+    :details "{{ details }}"
+    :time "{{ time }}"
+)
+"""
 
 today_date = datetime.datetime.now()
 tomorrow_date = today_date + datetime.timedelta(days=1)
@@ -9,7 +15,40 @@ tomorrow = tomorrow_date.strftime("%m/%d/%Y")
 print(today, tomorrow)
 
 stream = os.popen(
-    'gcalcli agenda "{}" "{}" --details end'.format(today, tomorrow)
+    'gcalcli agenda "{}" "{}" --details end --details calendar --tsv'.format(
+        today, tomorrow)
 )
 output = stream.read()
+
+unprocessed_tasks = output.split("\n")
+processed_tasks = []
+
+for task in unprocessed_tasks:
+    if task == '':
+        continue
+    task = task.split("\t")
+    task_dict = {
+        "start_date": task[0],
+        "start_time": task[1],
+        "end_date": task[2],
+        "end_time": task[3],
+        "details": task[4],
+        "calendar_source": task[5],
+    }
+    processed_tasks.append(task_dict)
+
+task_code = []
+
+for task in processed_tasks:
+    eww_task = yucky_event_template
+    eww_task = eww_task.replace("{{ details }}", task["details"])
+    eww_task = eww_task.replace(
+        "{{ time }}", "{} - {}".format(task["start_time"], task["end_time"])
+    )
+    print(eww_task)
+    task_code.append(eww_task)
+
 print(output)
+print(unprocessed_tasks)
+print(processed_tasks)
+# print(task_code)
