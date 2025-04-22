22c8538b2c5cc35a93522f020f090132898665c1
Timothy Karani <kartimothy@gmail.com>
2025-04-22 12:26:32 +0300

    Switched from phpactor to intelephense

--- configs/nvim/lua/plugins/lsp-spec.lua
+++ configs/nvim/lua/plugins/lsp-spec.lua
@@ -106,7 +106,7 @@ return {
 			})
 
 			require("mason-lspconfig").setup({
-				ensure_installed = { "pyright", "phpactor" },
+				ensure_installed = { "pyright", "intelephense" },
 				handlers = {
 					-- this first function is the "default handler"
 					-- it applies to every language server without a "custom handler"
